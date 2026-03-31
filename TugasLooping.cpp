9c0773b28251457dff68b60a86ed586a04744e35
nirayndra <faadiajasmine@gmail.com>
2026-03-31 14:48:50 +0700

    Menambah while loop untuk pengecekan prima

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -7,4 +7,9 @@ int pilihan;
 bool CekPrima() {
     if (angka < 2) return false;
     int i = 2;
+    while (i * i <= angka) {
+        if (angka % i == 0) return false;
+        i++;
+    }
+    return true;
 }
